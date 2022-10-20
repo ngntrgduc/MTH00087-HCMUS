a5df439323b133b96054194328137a20999877f8
ngntrgduc <trungducnguyen1511@gmail.com>
2022-10-20 18:14:20 +0700

    :sparkles: Add solutions of week 8

---
+++ Tuần 8/bai2.m
@@ -0,0 +1,7 @@
+coca = 850;
+pepsi = 720;
+nuoc = 600;
+conlai = 320;
+pie([coca, pepsi, nuoc, conlai]);
+title('Bieu do hinh quat minh hoa');
+legend('Coca', 'Pepsi', 'Nuoc tinh khiet', 'Loai khac')
