4a90ed5398e88a124ad9ece37b0b98ccb01ae41b
ngntrgduc <trungducnguyen1511@gmail.com>
2022-10-27 08:50:14 +0700

    Upload and update code

--- Tuần 8/bai2.m
+++ Tuần 8/bai2.m
@@ -4,4 +4,4 @@ nuoc = 600;
 conlai = 320;
 pie([coca, pepsi, nuoc, conlai]);
 title('Bieu do hinh quat minh hoa');
-legend('Coca', 'Pepsi', 'Nuoc tinh khiet', 'Loai khac')
+legend('Coca', 'Pepsi', 'Nuoc tinh khiet', 'Loai khac');
--- Tuần 8/bai5.m
+++ Tuần 8/bai5.m
@@ -3,7 +3,7 @@ x = sin(t);
 y = cos(t);
 z = t;
 plot3(x, y, z);
-title('Bai 5')
+title('Bai 5');
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
