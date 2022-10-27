bf0f60d3638689d32fbf8d66dde866ca6eabb313
ngntrgduc <trungducnguyen1511@gmail.com>
2022-10-27 08:49:09 +0700

    :recycle: Refactor code

--- Tuần 3/bai2_tonghop.m
+++ Tuần 3/bai2_tonghop.m
@@ -41,9 +41,9 @@ for i=1:length(v)
         tongchinhphuong = tongchinhphuong + v(i);
     end
 end
-    fprintf('tong so chan la %g\n', tongchan);
-    fprintf('tong so le la %g\n', tongle);
-    fprintf('tong so nguyen to chan la %g\n', tongntchan);
-    fprintf('tong so nguyen to le la %g\n', tongntle);
-    fprintf('tong so chinh phuong la %g\n', tongchinhphuong);
-        
+
+fprintf('tong so chan la %g\n', tongchan);
+fprintf('tong so le la %g\n', tongle);
+fprintf('tong so nguyen to chan la %g\n', tongntchan);
+fprintf('tong so nguyen to le la %g\n', tongntle);
+fprintf('tong so chinh phuong la %g\n', tongchinhphuong);        
--- Tuần 3/bai5.m
+++ Tuần 3/bai5.m
@@ -3,17 +3,17 @@ donvi = input('nhap don vi can doi: ');
 
 switch donvi
     case {'dm'}
-        fprintf('don vi sau khi doi la %g dm', L*10);
+        fprintf('don vi sau khi doi la %g dm', L * 10);
     case {'cm'}
-        fprintf('don vi sau khi doi la %g cm', L*100);
+        fprintf('don vi sau khi doi la %g cm', L * 100);
     case {'mm'}
-        fprintf('don vi sau khi doi la %g mm', L*1000);
+        fprintf('don vi sau khi doi la %g mm', L * 1000);
     case {'dam'}
-        fprintf('don vi sau khi doi la %g dam', L/10);
+        fprintf('don vi sau khi doi la %g dam', L / 10);
     case {'hm'}
-        fprintf('don vi sau khi doi la %g hm', L/100);
+        fprintf('don vi sau khi doi la %g hm', L / 100);
     case {'km'}
-        fprintf('don vi sau khi doi la %g km', L/1000);
+        fprintf('don vi sau khi doi la %g km', L / 1000);
     otherwise
         fprintf('don vi khong ton tai');
 end
