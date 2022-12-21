7f6bb44ef76dc5a8b2cd61aaa77f10b7f5f56c23
ngntrgduc <trungducnguyen1511@gmail.com>
2022-12-21 20:17:17 +0700

    :recycle: Refactor code

--- Tuần 3/bai4.m
+++ Tuần 3/bai4.m
@@ -1,9 +1,9 @@
 km = input('nhap quang duong di duoc: ');
 if (km < 2)
     fprintf('so tien phai tra la: %g', 15000 * km);
-elseif (km >= 2)&(km <=5)
+elseif (km >= 2) && (km <=5)
     fprintf('so tien phai tra la: %g', 13500 * km);
-elseif (km >= 6)&(km < 120)
+elseif (km >= 6) && (km < 120)
     fprintf('so tien phai tra la: %g', 11000 * km);
 elseif (km > 120)
     fprintf('so tien phai tra la: %2.f', 11000 * km * 90 / 100);
